3b803263a844d6f9b11a565b6ae7348bbe6073e2
CyberValya <valya7039@gmail.com>
2022-03-13 17:48:37 +0500

    create parser for auto.ru #parser-auto-ru

--- run_parser.py
+++ run_parser.py
@@ -2,7 +2,13 @@ from modules import Parser
 
 
 def run():
-    parser = Parser()
+    parser = Parser(
+        url='https://auto.ru/-/ajax/desktop/listing/',
+        out_dir='data',
+        start_price=100000,
+        stop_price=1000000
+    )
+    parser.get_cars()
 
 
 if __name__ == '__main__':
