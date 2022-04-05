2e335a2678ad256a656cb98fb27b83482a0336b4
m.e.perepilitsa <perepilica2905@gmail.com>
2022-04-05 12:40:33 +0500

    using sqlalchemy instead of pcycopg2 for db connection

--- src/modules/database.py
+++ src/modules/database.py
@@ -1,13 +1,15 @@
-import psycopg2
 import pandas as pd
+from sqlalchemy.engine import URL
+from sqlalchemy import create_engine, text, exc
 
 
 class DataBase:
     def __init__(self, connect_data):
-        self.connection = psycopg2.connect(**connect_data)
+        self.engine = create_engine(URL(**connect_data))
 
-    def select_query(self, sql):
-        return pd.read_sql_query(sql, self.connection)
-
-    def __del__(self):
-        self.connection.close()
+    def select_query(self, sql, params=None):
+        try:
+            data = pd.read_sql_query(text(sql), self.engine, params=params)
+            return True, data, None
+        except exc.SQLAlchemyError as error:
+            return False, None, error
