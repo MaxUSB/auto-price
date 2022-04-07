172dd256c1562ece6462e2745d1dfbdf2623bf90
CyberValya <valya7039@gmail.com>
2022-04-07 20:39:39 +0500

    modify module #catalog-filler

--- src/modules/catalog_filler.py
+++ src/modules/catalog_filler.py
@@ -13,36 +13,53 @@ class CatalogFiller:
             print('Connecting to database...')
             db = DataBase(connect_data=get_config('db')['catalogs'])
             print('Fill catalogs...')
-            self.__fill_auto_marks(db)
-            self.__fill_auto_capacity(db)
+            self.__fill_marks(db)
+            self.__fill_cities(db)
+            self.__fill_horse_powers(db)
             print('Done.')
         except:
             print('Connection failed')
 
-    def __fill_auto_marks(self, db):
-        auto_marks = list(filter(lambda item: item['table_name'] == 'marks', self.config['tables']))[0]
-        db.create_table(auto_marks)
+    def __fill_marks(self, db):
+        marks = list(filter(lambda item: item['table_name'] == 'marks', self.config['tables']))[0]
+        db.create_table(marks)
+        cols_list = list(filter(lambda item: item['name'] != 'id', marks['cols']))
+        db.insert_table(marks, cols_list, self.__get_text_col_formatted_data('Mark'))
 
+    def __fill_capacities(self, db):
+        capacities = list(filter(lambda item: item['table_name'] == 'capacities', self.config['tables']))[0]
+        db.create_table(capacities)
+        cols_list = list(filter(lambda item: item['name'] != 'id', capacities['cols']))
+        db.insert_table(capacities, cols_list, self.__get_mark_formatted_data(db, ['Mark', 'Capacity']))
+
+    def __fill_cities(self, db):
+        cities = list(filter(lambda item: item['table_name'] == 'cities', self.config['tables']))[0]
+        db.create_table(cities)
+        cols_list = list(filter(lambda item: item['name'] != 'id', cities['cols']))
+        db.insert_table(cities, cols_list, self.__get_text_col_formatted_data('City'))
+
+    def __fill_horse_powers(self, db):
+        horse_powers = list(filter(lambda item: item['table_name'] == 'horse_powers', self.config['tables']))[0]
+        db.create_table(horse_powers)
+        cols_list = list(filter(lambda item: item['name'] != 'id', horse_powers['cols']))
+        db.insert_table(horse_powers, cols_list, self.__get_mark_formatted_data(db, ['Mark', 'HP']))
+
+    def __get_text_col_formatted_data(self, col):
         data = []
-        for item in self.cars['Mark'].unique():
+        for item in self.cars[col].unique():
             data.append([f'\'{item}\''])
+        return data
 
-        cols_list = list(filter(lambda item: item['name'] != 'id', auto_marks['cols']))
-        db.insert_table(auto_marks, cols_list, data)
-
-    def __fill_auto_capacity(self, db):
-        auto_capacity = list(filter(lambda item: item['table_name'] == 'capacities', self.config['tables']))[0]
-        db.create_table(auto_capacity)
-
+    def __get_mark_formatted_data(self, db, cols):
         data = []
-        for i, row in self.cars[['Mark', 'Capacity']].drop_duplicates().iterrows():
+        for i, row in self.cars[cols].drop_duplicates().iterrows():
             values = []
             for value in row:
                 if isinstance(value, str):
                     select_query = f'''
-                        SELECT id from marks
-                        WHERE mark = '{value}'
-                    '''
+                                        SELECT id from marks
+                                        WHERE mark = '{value}'
+                                    '''
                     try:
                         auto = db.select_query(select_query)[1]['id'].values[0]
                         values.append(auto.__str__())
@@ -51,6 +68,4 @@ class CatalogFiller:
                 elif isinstance(value, int):
                     values.append(value.__str__())
             data.append(values)
-
-        cols_list = list(filter(lambda item: item['name'] != 'id', auto_capacity['cols']))
-        db.insert_table(auto_capacity, cols_list, data)
+        return data
