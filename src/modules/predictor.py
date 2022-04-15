5a4d466e51133c2e9aaf88cf51bc8e597430e38e
m.e.perepilitsa <perepilica2905@gmail.com>
2022-04-16 03:17:41 +0500

    split to 3 models by price segment

--- src/modules/predictor.py
+++ src/modules/predictor.py
@@ -11,9 +11,14 @@ class Predictor:
         self.features = self.config['features']
         self.filters = self.config['filters']
 
-        self.model = None
-        self.error_model = None
+        self.model_e = None
+        self.model_m = None
+        self.model_p = None
+        self.error_model_e = None
+        self.error_model_m = None
+        self.error_model_p = None
         self.custom_encode_dicts = {}
+        self.segments = pd.DataFrame()
 
     def __filter_data(self, data):
         for field in self.filters.keys():
@@ -58,6 +63,31 @@ class Predictor:
                 print(f'error: not found {encoding_type} encoding type handle', file=sys.stderr)
         return data
 
+    def __segment_split(self, data):
+        data_e = data[data['PriceSegment'] == 'ECONOMY'].drop(columns=['PriceSegment'])
+        data_m = data[data['PriceSegment'] == 'MEDIUM'].drop(columns=['PriceSegment'])
+        data_p = data[data['PriceSegment'] == 'PREMIUM'].drop(columns=['PriceSegment'])
+        models_e = data_e['Model'].unique().tolist()
+        models_m = data_m['Model'].unique().tolist()
+        models_p = data_p['Model'].unique().tolist()
+        self.segments = pd.concat([self.segments, pd.DataFrame({'Model': models_e, 'Segment': ['ECONOMY'] * len(models_e)})], ignore_index=True)
+        self.segments = pd.concat([self.segments, pd.DataFrame({'Model': models_m, 'Segment': ['MEDIUM'] * len(models_m)})], ignore_index=True)
+        self.segments = pd.concat([self.segments, pd.DataFrame({'Model': models_p, 'Segment': ['PREMIUM'] * len(models_p)})], ignore_index=True)
+        return data_e, data_m, data_p
+
+    def __get_segment_model(self, model):
+        segment = self.segments[self.segments['Model'] == model]['Segment']
+        if segment.empty:
+            return self.model_m, self.error_model_m
+        else:
+            segment = segment.iloc[0]
+        if segment == 'ECONOMY':
+            return self.model_e, self.error_model_e
+        elif segment == 'MEDIUM':
+            return self.model_m, self.error_model_m
+        elif segment == 'PREMIUM':
+            return self.model_p, self.error_model_p
+
     def fit(self, data):
         try:
             data = data[self.features.keys()]
@@ -67,22 +97,37 @@ class Predictor:
             self.__set_custom_field_encode_dicts(data)
             print('done.\nencoding fields...', end=' ')
             data = self.__encode_fields(data.copy())
-            print('done.\ncreating training pool...', end=' ')
-            x = data.drop(columns=self.target)
-            y = data[self.target]
-            print('done.\ncreating model...', end=' ')
-            self.model = Ridge()
-            print('done.\ntraining model...', end=' ')
-            self.model.fit(x, y)
-            print('done.\ncreating error model...', end=' ')
-            error_df = pd.DataFrame({
-                'real': y,
-                'predicted': self.model.predict(x)
-            })
-            error_df['error'] = error_df.apply(lambda row: abs(row['real'] - row['predicted']) / row['real'], axis=1)
-            self.error_model = Ridge()
-            print('done.\ntraining error model...', end=' ')
-            self.error_model.fit(x, error_df['error'])
+            print('done.\nsplitting data by segment...', end=' ')
+            data_e, data_m, data_p = self.__segment_split(data)
+            print('done.\ncreating training pools...', end=' ')
+            x_e = data_e.drop(columns=self.target)
+            x_m = data_m.drop(columns=self.target)
+            x_p = data_p.drop(columns=self.target)
+            y_e = data_e[self.target]
+            y_m = data_m[self.target]
+            y_p = data_p[self.target]
+            print('done.\ncreating models...', end=' ')
+            self.model_e = Ridge()
+            self.model_m = Ridge()
+            self.model_p = Ridge()
+            print('done.\ntraining models...', end=' ')
+            self.model_e.fit(x_e, y_e)
+            self.model_m.fit(x_m, y_m)
+            self.model_p.fit(x_p, y_p)
+            print('done.\ncreating error models...', end=' ')
+            error_df_e = pd.DataFrame({'real': y_e, 'predicted': self.model_e.predict(x_e)})
+            error_df_m = pd.DataFrame({'real': y_m, 'predicted': self.model_m.predict(x_m)})
+            error_df_p = pd.DataFrame({'real': y_p, 'predicted': self.model_p.predict(x_p)})
+            error_df_e['error'] = error_df_e.apply(lambda row: abs(row['real'] - row['predicted']) / row['real'], axis=1)
+            error_df_m['error'] = error_df_m.apply(lambda row: abs(row['real'] - row['predicted']) / row['real'], axis=1)
+            error_df_p['error'] = error_df_p.apply(lambda row: abs(row['real'] - row['predicted']) / row['real'], axis=1)
+            self.error_model_e = Ridge()
+            self.error_model_m = Ridge()
+            self.error_model_p = Ridge()
+            print('done.\ntraining error models...', end=' ')
+            self.error_model_e.fit(x_e, error_df_e['error'])
+            self.error_model_m.fit(x_m, error_df_m['error'])
+            self.error_model_p.fit(x_p, error_df_p['error'])
             print('done.')
             return True, None
         except Exception as e:
@@ -92,20 +137,22 @@ class Predictor:
         try:
             result = {}
             data[self.target] = 0
+            data['PriceSegment'] = 'UNKNOWN'
             data = data[self.features.keys()]
             print('filtering data...', end=' ')
             data = self.__filter_data(data.copy())
             print('done.\nencoding fields...', end=' ')
             data = self.__encode_fields(data.copy())
             data.drop(columns=self.target, inplace=True)
-            false_binary_columns = [col for col in self.model.feature_names_in_ if col not in data.columns]
+            model, error_model = self.__get_segment_model(data['Model'].iloc[0])
+            false_binary_columns = [col for col in model.feature_names_in_ if col not in data.columns]
             if false_binary_columns:
                 data[false_binary_columns] = 0
-            data = data[self.model.feature_names_in_]
+            data = data[model.feature_names_in_]
             print('done.\npredicting price...', end=' ')
-            result[self.target] = int(self.model.predict(data)[0])
+            result[self.target] = int(model.predict(data)[0])
             print('done.\npredicting error...', end=' ')
-            result['PredictedError'] = round(self.error_model.predict(data)[0], 2)
+            result[self.target] += int(result[self.target] * abs(round(error_model.predict(data)[0], 2)))
             print('done.')
             return True, result
         except Exception as e:
@@ -114,9 +161,14 @@ class Predictor:
 
     def store_model(self):
         model_data = {
-            'model': self.model,
-            'error_model': self.error_model,
+            'model_e': self.model_e,
+            'model_m': self.model_m,
+            'model_p': self.model_p,
+            'error_model_e': self.error_model_e,
+            'error_model_m': self.error_model_m,
+            'error_model_p': self.error_model_p,
             'custom_encode_dicts': self.custom_encode_dicts,
+            'segments': self.segments,
         }
         return save_data(model_data, 'predictor_model.pickle', 'models')
 
@@ -124,7 +176,12 @@ class Predictor:
         model_data = get_data('predictor_model.pickle', 'models')
         if model_data is None:
             return False
-        self.model = model_data['model']
-        self.error_model = model_data['error_model']
+        self.model_e = model_data['model_e']
+        self.model_m = model_data['model_m']
+        self.model_p = model_data['model_p']
+        self.error_model_e = model_data['error_model_e']
+        self.error_model_m = model_data['error_model_m']
+        self.error_model_p = model_data['error_model_p']
         self.custom_encode_dicts = model_data['custom_encode_dicts']
+        self.segments = model_data['segments']
         return True
